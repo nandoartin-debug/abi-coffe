13b369aaaa51935eb8d77392bbd66316443626c8
nandoartin-debug <nandoartin@gmail.com>
2025-10-23 21:53:28 +0700

    Update tes.js

--- tes.js
+++ tes.js
@@ -18,3 +18,10 @@ function revealOnScroll() {
         }
     }
 }
+const toggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+toggle.addEventListener("click", () => {
+    toggle.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
